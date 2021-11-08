a7d37a37fc886f07ec05b70f843d52d1a96fd7c6
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-08 03:11:41 +0000

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveMultiplier = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow =  Input.GetAxis("Horizontal");
-        float verticalThrow =  Input.GetAxis("Vertical");
-        
+        float xThrow =  Input.GetAxis("Horizontal");
+        float yThrow =  Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * moveMultiplier;
+        float xNewLocal = transform.localPosition.x + xOffset;
+
+        float yOffset = yThrow * Time.deltaTime * moveMultiplier;
+        float yNewLocal = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(xNewLocal, yNewLocal, zNewLocal);
     }
 }
