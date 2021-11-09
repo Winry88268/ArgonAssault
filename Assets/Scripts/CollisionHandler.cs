15cce71b4292d467a7bbae906124f9f73c04fc7f
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-09 01:52:56 +0000

    Minor Updates to Prefabs, Enemy Collision Handler Init

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    [Header("Generic Setup Settings")]
+    [Tooltip("Player Hits before Destruction")] [SerializeField] int hitPoints = 3;
+
+    void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+    }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other) 
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Destroy(gameObject);
+    }
+}
